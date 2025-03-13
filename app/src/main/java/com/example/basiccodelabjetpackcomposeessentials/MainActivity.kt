15d519e662a4bc9cf3672ac49654910f8d1d9c9f
Vinay Patel <vinayspatel0@gmail.com>
2025-03-13 16:15:50 +0000

    refactor: in Greeting function wrap Column in Row function. Add ElevatedButton in Greeting function

--- app/src/main/java/com/example/basiccodelabjetpackcomposeessentials/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelabjetpackcomposeessentials/MainActivity.kt
@@ -5,15 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsActions.OnClick
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccodelabjetpackcomposeessentials.ui.theme.BasicCodelabJetpackComposeEssentialsTheme
@@ -52,9 +55,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Text(text = "Hello ")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = {}) {
+                Text("Show more")
+            }
         }
     }
 }
