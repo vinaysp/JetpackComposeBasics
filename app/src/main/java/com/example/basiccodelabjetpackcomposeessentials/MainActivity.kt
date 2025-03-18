155e85cefe97bef7c80c5ec3f99e11fc2268e5c9
Vinay Patel <vinayspatel0@gmail.com>
2025-03-18 15:45:53 +0000

    feat: import Arrangement function

--- app/src/main/java/com/example/basiccodelabjetpackcomposeessentials/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelabjetpackcomposeessentials/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
