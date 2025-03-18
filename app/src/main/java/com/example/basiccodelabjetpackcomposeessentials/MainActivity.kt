a86760e6946fc87478c8bb81f1009c1951d9219a
Vinay Patel <vinayspatel0@gmail.com>
2025-03-18 15:45:36 +0000

    feat: import Arrangement and Button functions

--- app/src/main/java/com/example/basiccodelabjetpackcomposeessentials/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelabjetpackcomposeessentials/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -17,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.SemanticsActions.OnClick
 import androidx.compose.ui.tooling.preview.Preview
